35d6a8d70438c4bb0a2634c85cb40fa4ab37885b
MyChi <mychikhtn@gmail.com>
2026-03-10 19:32:40 +0700

    Chuyển Free HTML Template sang Express Handlebars Template

--- index.js
+++ index.js
@@ -3,13 +3,32 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const expressHandlebars = require("express-handlebars");
+const { partials } = require("handlebars");
 
 // cau hinh public static folder
 app.use(express.static(__dirname + "/public"));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello to EShop");
-// });
+// cau hinh su dung express-handlebars
+app.engine(
+  "hbs",
+  expressHandlebars.engine({
+    layoursDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials", // chứa layout con
+    extname: "hbs",
+    defaultLayout: "layout",
+  }),
+);
+app.set("view engine", "hbs");
+
+// routes
+app.get("/", (req, res) => {
+  res.render("index"); // nó lây nội dung trong file index.hbs này đổ vào trang layout.hbs
+});
+
+app.get("/:page", (req, res) => {
+  res.render(req.params.page);
+});
 
 // Khởi động web server
 app.listen(port, () => {
