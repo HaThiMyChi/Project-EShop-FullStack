e6764f45bbe4e88a5bf52450001f9b9e6d71c4e3
MyChi <mychikhtn@gmail.com>
2026-03-21 17:49:05 +0700

    Hiển thị danh sách theo Brand

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -9,10 +9,18 @@ controller.show = async (req, res) => {
 
   let category = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
 
+  let brand = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
+
   let categories = await models.Category.findAll({
     include: [{ model: models.Product }], // muốn lấy thêm 1 categories có những sản phẩm nào, vì trong bảng product có khóa ngoại categoryId nên có thể lấy được những sản phẩm nào thuộc về category đó, nếu không có thì sẽ không lấy được
   });
   res.locals.categories = categories;
+
+  let brands = await models.Brand.findAll({
+    include: [{ model: models.Product }],
+  });
+  // truyen brands nay len view
+  res.locals.brands = brands;
 
   let options = {
     attributes: ["id", "name", "imagePath", "stars", "price", "oldPrice"],
@@ -21,6 +29,10 @@ controller.show = async (req, res) => {
 
   if (category > 0) {
     options.where.categoryId = category;
+  }
+
+  if (brand > 0) {
+    options.where.brandId = brand;
   }
 
   let products = await models.Product.findAll(options);
