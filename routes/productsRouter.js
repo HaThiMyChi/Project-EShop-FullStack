b8fe998f09a06e7288ea5aed560ab2e4c6d6e37a
MyChi <mychikhtn@gmail.com>
2026-04-02 20:18:34 +0700

    Hiển thị chi tiết giỏ hàng

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -6,6 +6,7 @@ let controller = require("../controllers/productsController");
 let cartController = require("../controllers/cartController");
 
 router.get("/", controller.getData, controller.show);
+router.get("/cart", cartController.show);
 router.get("/:id", controller.getData, controller.showDetail); // nó truyền trên đường dẫn theo dạng id khi lấy thì dùng params, còn truyền theo dạng href thì lấy theo query
 
 router.post("/cart", cartController.add);
