0f4ecb3287925a4350fb71d904b5e25fdd29d22d
MyChi <mychikhtn@gmail.com>
2026-04-15 22:13:49 +0700

    Duy trì trạng thái Giỏ hàng sau khi Login và Logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,9 @@ controller.show = (req, res) => {
 
 controller.login = (req, res, next) => {
   let keepSignedIn = req.body.keepSignedIn;
+  // lay cart tu session
+  let cart = req.session.cart;
+
   // Passport sẽ tìm strategy đã đăng ký tên:local-login
   passport.authenticate("local-login", (error, user) => {
     if (error) {
@@ -27,17 +30,22 @@ controller.login = (req, res, next) => {
         return next(error);
       }
       req.session.cookie.maxAge = keepSignedIn ? 24 * 60 * 60 * 1000 : null; // 1 ngay
+      req.session.cart = cart; // sau khi dang nhap thanh cong thi se gan cart vao session de tiep tuc su dung
       return res.redirect("/users/my-account");
     });
   })(req, res, next); // đang gọi middleware đó ngay lập tức.
 };
 
 controller.logout = (req, res, next) => {
+  let cart = req.session.cart; // luu lai cart truoc khi logout
   req.logout((error) => {
     if (error) {
       return next(error);
     }
     // neu khong co loi thi se chuyen huong ve trang chu
+
+    // sau khi logout xong thi se gan cart vao session de tiep tuc su dung
+    req.session.cart = cart;
     res.redirect("/");
   });
 };
